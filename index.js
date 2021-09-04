67e74cf704f23c0e3c5b0b55eba89d686a39b368
Lunkaz <marialbornozapata@gmail.com>
2021-09-03 23:24:53 -0300

    Improving save function

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 let countEl = document.getElementById("count-el");
+let saveEl = document.getElementById("save-el");
 let count = 0;
+let dash = " - "
 
 
 function increment() {
-    count = count + 1;
-    countEl.innerText = count;
+    count += 1;
+    countEl.textContent = count;
     
 }
 
 function save() {
-    console.log(count);
+    let countStr = count + " - ";
+    saveEl.textContent += countStr;
+    countEl.textContent = 0;
+    count = 0;
 }
