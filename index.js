86bba206446e77a42ccfbfe55de6c20aa9c91aa1
Lunkaz <marialbornozapata@gmail.com>
2021-09-04 19:32:34 -0300

    Event listeners & total count

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+const incrementBtn = document.getElementById("increment-btn");
+const saveBtn = document.getElementById("save-btn");
 let countEl = document.getElementById("count-el");
 let saveEl = document.getElementById("save-el");
+const totalEl = document.getElementById("total-el");
 let count = 0;
-let dash = " - "
+let total = 0;
 
 
 function increment() {
@@ -14,5 +17,11 @@ function save() {
     let countStr = count + " - ";
     saveEl.textContent += countStr;
     countEl.textContent = 0;
+    total += count;
+    totalEl.textContent = total;
     count = 0;
 }
+console.log(totalEl);
+
+incrementBtn.addEventListener('click', increment);
+saveBtn.addEventListener('click', save);
